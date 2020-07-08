3d4373888ab93d923f34e90850dd6d5134d6379b
GeraldRyan <gerald.ryan40@yahoo.com>
2020-07-08 15:36:50 -0500

    got expensive sequence figured out

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,8 +1,18 @@
 # Your code here
 
-
+cache = {}
 def expensive_seq(x, y, z):
     # Your code here
+    
+    h = hash((x,y,z))
+    if h not in cache:
+        if x <= 0:
+            cache[h] = y + z
+        else:
+            cache[h] = expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
+    return cache[h]
+
+
 
 
 
