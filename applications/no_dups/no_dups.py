400cf1530e4c6ad9c105875612daac3d71465822
GeraldRyan <gerald.ryan40@yahoo.com>
2020-07-08 18:10:51 -0500

    got "remove duplicates" working

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,24 @@
 def no_dups(s):
     # Your code here
 
+    # I think this is very bad code but it's working nonetheless. 
+    duplicate_tracker = {}
+
+    listify = s.split()
+    final_list = []
+    for word in listify:
+        if word in duplicate_tracker:
+            next 
+        else:
+            final_list.append(word)
+            duplicate_tracker[word] = word
+
+    
+    string = " ".join(final_list)
+
+
+    return string
+
 
 
 if __name__ == "__main__":
