35b52a13087409a416063bd73ec724f50f1f778c
GeraldRyan <gerald.ryan40@yahoo.com>
2020-07-08 15:24:44 -0500

    got the lookup table cache memoization working

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,7 +1,14 @@
 # Your code here
+import random, math
+from datetime import datetime
+start=datetime.now()
 
 
-def slowfun_too_slow(x, y):
+
+
+cache = {}
+
+def slowfun_is_too_slow(x, y):
     v = math.pow(x, y)
     v = math.factorial(v)
     v //= (x + y)
@@ -15,12 +22,24 @@ def slowfun(x, y):
     output, but completes quickly instead of taking ages to run.
     """
     # Your code here
+    h = hash((x,y))
 
+    if h not in cache:
+        v = math.pow(x, y)
+        v = math.factorial(v)
+        v //= (x + y)
+        v %= 982451653
+        cache[h] = v
+
+    return cache[h]
 
 
 # Do not modify below this line!
 
 for i in range(50000):
-    x = random.randrange(2, 14)
+    x = random.randrange(2, 14) 
     y = random.randrange(3, 6)
     print(f'{i}: {x},{y}: {slowfun(x, y)}')
+
+print(f'time is ${datetime.now()-start}')
+
