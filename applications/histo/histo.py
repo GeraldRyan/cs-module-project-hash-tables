0302f8cb8303fc9b1f99383a379453e95a348d5a
GeraldRyan <gerald.ryan40@yahoo.com>
2020-07-09 18:51:04 -0500

    getting started with histogram

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,8 +1,19 @@
 # Your code here
 
+def filter_string(s1, s2):
+  for c in s2:
+    s1 = s1.replace(c, '')
+    return s1
+
+
+
+
 with open("robin.txt") as f:
   words = f.read()
 
 
 
-print(words)
+# print(words)
+
+filtered_words = filter_string(words, "e")
+print(filtered_words)
