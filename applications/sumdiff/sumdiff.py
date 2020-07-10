cb2503d80b556e636f21ddbe0a4c798fc5bbb417
GeraldRyan <gerald.ryan40@yahoo.com>
2020-07-09 19:52:00 -0500

    beginning sum-diff

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -3,13 +3,24 @@ find all a, b, c, d in q such that
 f(a) + f(b) = f(c) - f(d)
 """
 
-#q = set(range(1, 10))
-#q = set(range(1, 200))
-q = (1, 3, 4, 7, 12)
 
+q = (1, 3, 4, 7, 12)
 
 def f(x):
     return x * 4 + 6
 
 # Your code here
+computed_values = {}
+
+def sum_diff(tup):
+    for t in tup:
+        if t not in computed_values:
+            computed_values[t] = f(t)
+    return computed_values
+
+computed_values = sum_diff(q)
+print(computed_values)
+
+for c in computed_values:
+    print(c, computed_values[c])
 
